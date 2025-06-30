395d92bfecd73cb36b4c55eb09b0e4a5d25e8e59
ahpicl64 <ahpicl64@gmail.com>
2025-06-30 21:18:24 +0900

    fix: 오탈자 수정

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   output: 'export', // S3 정적파일 배포위한 설정 추가 (npm build run시 정적파일이 ./out 폴더에 생성)
   eslint: {
       ignoreDuringBuilds: true, // (임시) 빌드 시 ESLint 검사 건너뛰기
-      }
+      },
   images: {
     remotePatterns: [
       {
