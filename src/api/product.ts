0191e17da657728a9b8143ad30b6755a7dd8bbde
sihyun10 <hyhy200170@gmail.com>
2025-07-02 01:02:03 +0900

    feat: 카테고리 탭 상품 조회 API 및 타입 정의 추가 #9
    카테고리 탭 상품 조회할 API와 타입을 정의해두었다.

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,5 +1,6 @@
 import { axiosWithAuth } from "@/api";
 import type { MainProductResponse } from "@/types/product";
+import type { CategoryProductResponse } from "@/types/product";
 
 /* axiosWithAuth() : 인증된 사용자 */
 
@@ -10,3 +11,19 @@ export const fetchMainProducts = async (): Promise<MainProductResponse> => {
   return response.data;
 };
 
+// 카테고리 탭 화면 상품 불러오기
+export const fetchCategoryProducts = async (
+  categoryId: number,
+  page = 0,
+  size = 10
+): Promise<CategoryProductResponse> => {
+  const response = await axiosWithAuth().get("/api/home/products/category", {
+    params: {
+      categoryId,
+      page,
+      size,
+    },
+  });
+
+  return response.data;
+};
