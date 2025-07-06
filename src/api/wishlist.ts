88f1297fc44cbf588059d1b4f27ab1036d1daed5
sihyun10 <hyhy200170@gmail.com>
2025-07-07 07:55:23 +0900

    refactor: 찜 기능과 북마크 관련 파일 제거 #26
    옷장 테이블을 새로 만들면서 북마크 관련 파일은 제거하였다.
    또한 위시리스트 찜 기능도 개선하였다.

--- src/api/wishlist.ts
+++ src/api/wishlist.ts
@@ -1,12 +1,29 @@
 import { axiosWithAuth } from "@/api";
 import { DeleteWishlist, PostWishlist } from "@/types/wishlist";
+import { getAccessToken } from "@/utils/auth";
 
 export const addWishlist = async (data: PostWishlist) => {
-  const response = await axiosWithAuth().post("/api/wishlist/add", data);
+  // 토큰 확인
+  const token = getAccessToken();
+  if (!token) {
+    throw new Error("로그인이 필요합니다.");
+  }
+
+  const response = await axiosWithAuth().post(
+    `/api/wishlist/add?productId=${data.productId}`
+  );
   return response.data;
 };
 
 export const removeWishlist = async (data: DeleteWishlist) => {
-  const response = await axiosWithAuth().post("/api/wishlist/remove", data);
+  // 토큰 확인
+  const token = getAccessToken();
+  if (!token) {
+    throw new Error("로그인이 필요합니다.");
+  }
+
+  const response = await axiosWithAuth().delete(
+    `/api/wishlist/remove?productId=${data.productId}`
+  );
   return response.data;
 };
