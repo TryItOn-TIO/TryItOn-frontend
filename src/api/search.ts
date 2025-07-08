b6c3af1d5651832e4a8f3e43573c115a4ed45849
sihyun10 <hyhy200170@gmail.com>
2025-07-08 19:56:30 +0900

    refactor: 인증이 필요없는 API 요청으로 변경 #49
    다시 생각해보니 검색 기능은 로그인 여부 상관이 없다고 판단하에
    axiosWithAuth를 쓰지않고 axiosWithoutAuth를 사용하여 불필요한 토큰 없이
    API 요청을 할 수 있도록 변경해주었다.

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,31 +1,17 @@
-import { axiosWithAuth, axiosWithoutAuth } from "@/api";
+import { axiosWithoutAuth } from "@/api";
 import type { SearchProductResponse } from "@/types/product";
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
 
 /**
  * 자동완성 검색어 추천 요청
- * @param query 검색어
  */
 export const fetchSearchSuggestions = async (
   query: string
 ): Promise<string[]> => {
-  const token =
-    typeof window !== "undefined" ? localStorage.getItem("accessToken") : null;
-
   try {
-    if (token) {
-      const response = await axiosWithAuth().get("/api/products/suggestions", {
-        params: { query },
-      });
-      return response.data;
-    } else {
-      const response = await axiosWithoutAuth().get(
-        `/api/products/suggestions`,
-        { params: { query } }
-      );
-      return response.data;
-    }
+    const response = await axiosWithoutAuth().get("/api/products/suggestions", {
+      params: { query },
+    });
+    return response.data;
   } catch (error) {
     console.warn("자동완성 요청 실패:", error);
     return [];
@@ -33,27 +19,16 @@ export const fetchSearchSuggestions = async (
 };
 
 /**
- * 검색어에 해당하는 상품 목록 요청
- * @param query 검색어
+ * 검색 결과 요청
  */
 export const fetchSearchResults = async (
   query: string
 ): Promise<SearchProductResponse> => {
-  const token =
-    typeof window !== "undefined" ? localStorage.getItem("accessToken") : null;
-
   try {
-    if (token) {
-      const response = await axiosWithAuth().get("/api/products/search", {
-        params: { query },
-      });
-      return response.data;
-    } else {
-      const response = await axiosWithoutAuth().get("/api/products/search", {
-        params: { query },
-      });
-      return response.data;
-    }
+    const response = await axiosWithoutAuth().get("/api/products/search", {
+      params: { query },
+    });
+    return response.data;
   } catch (error) {
     console.warn("검색 결과 요청 실패:", error);
     return { products: [], totalCount: 0 };
