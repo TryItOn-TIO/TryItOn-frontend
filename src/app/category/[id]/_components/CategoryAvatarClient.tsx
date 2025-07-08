25d107ef6ca0396ca5ec65b4d874471cc81c448e
hyeona01 <sha0209@dgu.ac.kr>
2025-07-08 16:42:53 +0900

    feat: 비로그인 유저 아바타 섹션 컴포넌트화

--- src/app/category/[id]/_components/CategoryAvatarClient.tsx
+++ src/app/category/[id]/_components/CategoryAvatarClient.tsx
@@ -7,20 +7,17 @@ import { useAvatarStore } from "@/stores/avatar-store";
 import { getAccessToken } from "@/utils/auth";
 import { fetchLatestAvatarInfo } from "@/api/avatar";
 import AvatarWearInfo from "@/components/common/AvatarWearInfo";
+import DefaultAvatarSlot from "@/components/common/DefaultAvatarSlot";
 
 const CategoryAvatarClient = () => {
   const setAvatarImg = useAvatarStore((state) => state.setAvatarImg);
 
+  const token = getAccessToken();
+  const isLoggedIn = !!token;
+
   useEffect(() => {
     const loadAvatarInfo = async () => {
       try {
-        const token = getAccessToken();
-        if (!token) {
-          // 로그인하지 않은 경우 기본 아바타 이미지 설정
-          setAvatarImg("/images/default-avatar.png");
-          return;
-        }
-
         const data = await fetchLatestAvatarInfo();
         setAvatarImg(data.avatarImg); // 전역 상태 업데이트
       } catch (error) {
@@ -33,8 +30,11 @@ const CategoryAvatarClient = () => {
     loadAvatarInfo();
   }, [setAvatarImg]);
 
-  // AvatarWearInfo 컴포넌트는 내부적으로 avatar store를 사용하므로 props 없이 사용
-  return <AvatarWearInfo />;
+  if (isLoggedIn) {
+    return <AvatarWearInfo />;
+  } else {
+    return <DefaultAvatarSlot />;
+  }
 };
 
 export default CategoryAvatarClient;
