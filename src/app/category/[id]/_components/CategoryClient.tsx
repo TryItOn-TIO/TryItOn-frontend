0d3cb4874f28e51e4cabf5e9002a81f4e8e74661
hyeona01 <sha0209@dgu.ac.kr>
2025-07-18 16:46:58 +0900

    feat: 하위 카테고리 UI 추가 #81

--- src/app/category/[id]/_components/CategoryClient.tsx
+++ src/app/category/[id]/_components/CategoryClient.tsx
@@ -2,16 +2,42 @@
 
 "use client";
 
-import React from "react";
+import React, { useMemo, useState } from "react";
 import CategoryProductList from "@/app/category/[id]/_components/CategoryProductList";
+import CategoryFilter from "@/app/closet/_components/CategoryFilter";
+import { child_category } from "@/constants/category";
 
 type Props = {
   categoryId: number;
 };
 
 const CategoryClient = ({ categoryId }: Props) => {
+  // const categories = ["전체", "긴소매 티셔츠", "반소매 티셔츠"];
+  const [selectedCategory, setSelectedCategory] = useState("전체");
+
+  const categories = useMemo(() => {
+    const filteredChildCategories = child_category.filter(
+      (item) => item.category_id === categoryId
+    );
+
+    const categoryNames = filteredChildCategories.map(
+      (item) => item.categoryName
+    );
+
+    return ["전체", ...categoryNames];
+  }, [categoryId]);
+
+  const handleCategoryChange = (category: string) => {
+    setSelectedCategory(category);
+  };
+
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 space-y-5">
+      <CategoryFilter
+        categories={categories}
+        selectedCategory={selectedCategory}
+        onCategoryChange={handleCategoryChange}
+      />
       <CategoryProductList categoryId={categoryId} />
     </div>
   );
