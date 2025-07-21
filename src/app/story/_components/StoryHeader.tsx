39257d06745bbd567dd8e12859a68567e2d95055
hyeona01 <sha0209@dgu.ac.kr>
2025-07-22 01:24:55 +0900

    fix: 스토리 페이지 모바일 뷰에서 깨지는 현상 해결 #81

--- src/app/story/_components/StoryHeader.tsx
+++ src/app/story/_components/StoryHeader.tsx
@@ -9,23 +9,21 @@ type StoryHeaderProps = {
 const StoryHeader = ({ sortType, onSortChange }: StoryHeaderProps) => {
   return (
     <>
-      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-4">
-        <h1 className="text-2xl font-bold text-gray-900 mb-4 sm:mb-0">
-          스토리
-        </h1>
+      <div className="flex justify-between items-start mb-4">
+        <h1 className="text-2xl font-bold text-gray-900">스토리</h1>
 
-        <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
+        <div className="flex gap-2">
           {/* 스토리 관리 버튼 */}
           <Link
             href="/mypage/story"
-            className="px-6 py-2 bg-gray-200 text-gray-800 rounded-lg hover:bg-gray-300 transition-colors text-center"
+            className="px-4 py-2 bg-gray-200 text-gray-800 rounded-lg hover:bg-gray-300 transition-colors text-center"
           >
             스토리 관리
           </Link>
           {/* 스토리 작성 버튼 */}
           <Link
             href="/story/create"
-            className="px-6 py-2 bg-black text-white rounded-lg hover:bg-gray-700 transition-colors text-center"
+            className="px-4 py-2 bg-black text-white rounded-lg hover:bg-gray-700 transition-colors text-center"
           >
             스토리 작성
           </Link>
